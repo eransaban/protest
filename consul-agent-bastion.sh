021f56805cf16608d75951e8526cae67204bd9c0
Eran Saban <eransaban@gmail.com>
2020-03-28 12:17:31 +0300

    test

---
+++ consul-agent-bastion.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+set -e
+
+### set consul version
+CONSUL_VERSION="1.6.2"
+### set Node Exporter Version
+PROMETHEUS_DIR="/opt/prometheus"
+NODE_EXPORTER_VERSION-"0.18.1"
+
+echo "Grabbing IPs..."
+PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+echo "Installing dependencies..."
+apt-get -qq update &>/dev/null
+apt-get -yqq install unzip dnsmasq &>/dev/null
+#apt-get -qq install apache2
+
+echo "Configuring dnsmasq..."
+cat << EODMCF >/etc/dnsmasq.d/10-consul
+# Enable forward lookup of the 'consul' domain:
+server=/consul/127.0.0.1#8600
+EODMCF
+echo "Change Systemd-Resolved to Allow Ping and host"
+cat << EODMCF >>/etc/systemd/resolved.conf
+# Enable Systemd-Resolved find local domains:
+DNS=127.0.0.1
+Domains=~consul
+EODMCF
+
+systemctl restart dnsmasq
+systemctl restart systemd-resolved
+
+echo "Fetching Consul..."
+cd /tmp
+curl -sLo consul.zip https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+
+echo "Installing Consul..."
+unzip consul.zip >/dev/null
+chmod +x consul
+mv consul /usr/local/bin/consul
+
+# Setup Consul
+mkdir -p /opt/consul
+mkdir -p /etc/consul.d
+mkdir -p /run/consul
+tee /etc/consul.d/config.json > /dev/null <<EOF
+{
+  "advertise_addr": "$PRIVATE_IP",
+  "data_dir": "/opt/consul",
+  "datacenter": "saban",
+  "encrypt": "uDBV4e+LbFW3019YKPxIrg==",
+  "disable_remote_exec": true,
+  "disable_update_check": true,
+  "leave_on_terminate": true,
+  "retry_join": ["provider=aws tag_key=consul_server tag_value=true"],
+  "enable_script_checks": true,
+  "server": false
+  }
+EOF
+
+# Create user & grant ownership of folders
+useradd consul
+chown -R consul:consul /opt/consul /etc/consul.d /run/consul
+
+
+# Configure consul service
+tee /etc/systemd/system/consul.service > /dev/null <<"EOF"
+[Unit]
+Description=Consul service discovery agent
+Requires=network-online.target
+After=network.target
+
+[Service]
+User=consul
+Group=consul
+PIDFile=/run/consul/consul.pid
+Restart=on-failure
+Environment=GOMAXPROCS=2
+#ExecStartPre=[ -f "/run/consul/consul.pid" ] && /usr/bin/rm -f /run/consul/consul.pid
+ExecStart=/usr/local/bin/consul agent -pid-file=/run/consul/consul.pid -config-dir=/etc/consul.d
+ExecReload=/bin/kill -s HUP $MAINPID
+KillSignal=SIGINT
+TimeoutStopSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+#configure service and health check 
+echo '{
+  "check": {
+    "id": "ssh",
+    "name": "SSH TCP on port 22",
+    "tcp": "localhost:22",
+    "interval": "10s",
+    "timeout": "1s"
+  }
+}' > /etc/consul.d/bastion.json
+
+
+
+
+systemctl daemon-reload
+systemctl enable consul.service
+systemctl start consul.service
+consul reload
+
+### Install Node Exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz -O /tmp/node_exporter.tgz
+mkdir -p ${PROMETHEUS_DIR}
+tar zxf /tmp/node_exporter.tgz -C ${PROMETHEUS_DIR}
+
+# Configure node exporter service
+tee /etc/systemd/system/node_exporter.service > /dev/null <<EOF
+[Unit]
+Description=Prometheus node exporter
+Requires=network-online.target
+After=network.target
+
+[Service]
+ExecStart=${PROMETHEUS_DIR}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter
+KillSignal=SIGINT
+TimeoutStopSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable node_exporter.service
+systemctl start node_exporter.service
